6102e6a716bbefebe26c760edf4cae57ac09c28d
smoonh22 <smoonh22@gmail.com>
2019-09-23 22:16:28 +0900

    add basic controller integration test

---
+++ src/test/java/com/mastering/spring/springboot/BasicControllerIT.java
@@ -0,0 +1,34 @@
+package com.mastering.spring.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BasicControllerIT {
+    private static final String LOCAL_HOST = "http://localhost:";
+
+    @LocalServerPort
+    private int port;
+    private TestRestTemplate template = new TestRestTemplate();
+
+    @Test
+    public void welcome() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(createURL("/welcome"), String.class);
+        assertThat(response.getBody(), equalTo("Hello World"));
+
+    }
+
+    private String createURL(String uri) {
+        return LOCAL_HOST + port + uri;
+    }
+
+}
