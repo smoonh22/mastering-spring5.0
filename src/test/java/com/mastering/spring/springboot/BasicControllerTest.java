49f35209e5a7ca16a3413c368dfe074f1dbea8b4
smoonh22 <smoonh22@gmail.com>
2019-09-23 22:05:13 +0900

    add basic controller test

---
+++ src/test/java/com/mastering/spring/springboot/BasicControllerTest.java
@@ -0,0 +1,33 @@
+package com.mastering.spring.springboot;
+
+import com.mastering.spring.springboot.conroller.BasicController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BasicController.class)
+public class BasicControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void welcome() throws Exception {
+        mvc.perform(
+                MockMvcRequestBuilders.get("/welcome")
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("Hello World")));
+    }
+}
